d0e16205be918b2df20d96331f7ce41e5db1e6eb
Mithilesh Tiwari <Mithilesh@crowdbotics.com>
2022-10-12 22:47:26 +0530

    Crowdbotics Studio: Update app screens

---
+++ screens/Untitled12/index.js
@@ -0,0 +1,64 @@
+import { Pressable } from "react-native";
+import { Text } from "react-native";
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+const Untitled12 = () => {
+  return <View style={_styles.BxdRtUSA}>
+      <View style={_styles.xNyXhlsW}></View><Text style={_styles.hFdEsibN}>Lorem ipsum…</Text><Text style={_styles.awChkucI}>Lorem ipsum…</Text><Pressable onPress={{}} style={_styles.qdKvfaFM}><View style={_styles.cIZKOeMk}></View></Pressable></View>;
+};
+
+export default Untitled12;
+
+const _styles = StyleSheet.create({
+  BxdRtUSA: {
+    backgroundColor: "#f0f0f1",
+    padding: 10,
+    position: "relative",
+    height: "100%"
+  },
+  xNyXhlsW: {
+    left: 114,
+    top: 65,
+    position: "absolute",
+    height: 60,
+    width: 140,
+    backgroundColor: "#E4E4E4",
+    borderRadius: 0,
+    color: "#777777"
+  },
+  hFdEsibN: {
+    left: 116,
+    top: 226,
+    position: "absolute",
+    width: 100,
+    height: 50,
+    lineHeight: 14,
+    fontSize: 14,
+    borderRadius: 0
+  },
+  awChkucI: {
+    left: 47,
+    top: 392,
+    position: "absolute",
+    width: 241,
+    height: 80,
+    lineHeight: 14,
+    fontSize: 14,
+    borderRadius: 0,
+    transform: "rotate(90deg)"
+  },
+  cIZKOeMk: {
+    left: 114,
+    top: 135,
+    position: "absolute",
+    height: 60,
+    width: 140,
+    backgroundColor: "#E4E4E4",
+    borderRadius: 0,
+    color: "#777777"
+  },
+  qdKvfaFM: {
+    position: "unset"
+  }
+});
